1bf99aabe101cac15eed468c6fac3a20d43cbb93
Maximo <maximorossini2016@gmail.com>
2022-06-15 23:17:09 -0300

    dwe

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -2,7 +2,7 @@ import Cors from "cors";
 import initMiddleware from "../../lib/init-middleware";
 
 // Initialize the cors middleware
-const cors = initMiddleware(
+export const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
